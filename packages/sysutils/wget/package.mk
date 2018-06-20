78d457606f3a8e31d9a8160c86376d89a7ef6ad4
alexelec <alexelecv5@gmail.com>
2018-06-20 18:42:55 +0300

    build and other fix

--- packages/sysutils/wget/package.mk
+++ packages/sysutils/wget/package.mk
@@ -5,6 +5,7 @@
 
 PKG_NAME="wget"
 PKG_VERSION="1.18"
+PKG_SHA256="b5b55b75726c04c06fe253daec9329a6f1a3c0c1878e3ea76ebfebc139ea9cc1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.wget-editor.org/"
