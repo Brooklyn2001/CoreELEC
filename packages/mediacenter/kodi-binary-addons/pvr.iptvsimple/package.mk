06f8e477211ae9994c3316e13d165050237b7841
alexelec <alexelecv5@gmail.com>
2018-06-23 14:10:07 +0300

    pvr.iptvsimple: fix addon

--- packages/mediacenter/kodi-binary-addons/pvr.iptvsimple/package.mk
+++ packages/mediacenter/kodi-binary-addons/pvr.iptvsimple/package.mk
@@ -1,23 +1,11 @@
 ################################################################################
-#      This file is part of LibreELEC - https://libreelec.tv
-#      Copyright (C) 2018-present Team LibreELEC
-#      Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
-#
-#  LibreELEC is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 2 of the License, or
-#  (at your option) any later version.
-#
-#  LibreELEC is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+#      This file is part of Alex@ELEC - http://www.alexelec.in.ua
+#      Copyright (C) 2011-present Alexandr Zuyev (alex@alexelec.in.ua)
 ################################################################################
 
 PKG_NAME="pvr.iptvsimple"
+#PKG_VERSION="663040c"
+#PKG_SHA256="6be5a4dcf368924a04cd458b9b3f264336e8cdb68a2fca77fe26673f224798a7"
 PKG_VERSION="7db634b"
 PKG_SHA256="8455a157001dfd1fac24ea86ebe03c4b36c62b5a981d58f90794cc65df5d84e4"
 PKG_REV="3"
