e2ebd66bb77b3bf0c60cd8d1545bed3d09d34d7c
alexelec <alexelecv5@gmail.com>
2018-07-16 12:55:44 +0300

    packages/multimedia: Merge remote-tracking branch 'LibreELEC/master'

--- packages/multimedia/libmpeg2/package.mk
+++ packages/multimedia/libmpeg2/package.mk
@@ -1,6 +1,7 @@
 ################################################################################
 #      This file is part of LibreELEC - https://libreelec.tv
 #      Copyright (C) 2018-present Team LibreELEC
+#      Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
 #
 #  LibreELEC is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -16,17 +17,19 @@
 #  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
-PKG_NAME="aom"
-PKG_VERSION="96ee0eb"
-PKG_SHA256="37c8d930cc105ccad4987c65751400fc42819b77fd487c9ef19cadee0c95a2d8"
+PKG_NAME="libmpeg2"
+PKG_VERSION="0.5.1"
+PKG_SHA256="dee22e893cb5fc2b2b6ebd60b88478ab8556cb3b93f9a0d7ce8f3b61851871d4"
 PKG_ARCH="any"
-PKG_LICENSE="BSD"
-PKG_SITE="https://www.webmproject.org"
-PKG_URL="http://repo.or.cz/aom.git/snapshot/${PKG_VERSION}.tar.gz"
+PKG_LICENSE="GPLv2"
+PKG_SITE="http://libmpeg2.sourceforge.net/"
+PKG_URL="http://libmpeg2.sourceforge.net/files/$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
-PKG_SECTION="multimedia"
-PKG_LONGDESC="AV1 Codec Library"
+PKG_LONGDESC="The MPEG Library is a collection of C routines to decode MPEG-1 and MPEG-2 movies."
 
-PKG_CMAKE_OPTS_TARGET="-DENABLE_CCACHE=1 \
-                       -DENABLE_DOCS=0 \
-                       -DENABLE_TOOLS=0"
+PKG_CONFIGURE_OPTS_TARGET="--disable-sdl \
+                           --without-x"
+
+post_makeinstall_target() {
+  rm -rf $INSTALL/usr/bin
+}
