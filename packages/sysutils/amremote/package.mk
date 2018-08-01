d8f493d394b56b718af886b7031ad7fb90b4aae9
alexelec <alexelecv5@gmail.com>
2018-08-01 12:51:19 +0300

    amremote: enable pkg

--- packages/sysutils/amremote/package.mk
+++ packages/sysutils/amremote/package.mk
@@ -1,20 +1,5 @@
-################################################################################
-#      This file is part of OpenELEC - http://www.openelec.tv
-#      Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
-#
-#  OpenELEC is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 2 of the License, or
-#  (at your option) any later version.
-#
-#  OpenELEC is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
-################################################################################
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2011-present AlexELEC (http://www.alexelec.in.ua)
 
 PKG_NAME="amremote"
 PKG_VERSION="6431040"
@@ -32,13 +17,6 @@ makeinstall_target() {
   mkdir -p $INSTALL/usr/bin
     cp remotecfg $INSTALL/usr/bin
 
-  mkdir -p $INSTALL/usr/lib/coreelec
-    cp $PKG_DIR/scripts/* $INSTALL/usr/lib/coreelec
-
-  mkdir -p $INSTALL/etc/amremote
-    cp $PKG_DIR/config/*.conf $INSTALL/etc/amremote
-}
-
-post_install() {
-  enable_service amlogic-remotecfg.service
+  mkdir -p $INSTALL/usr/config/amremote
+    cp $PKG_DIR/config/* $INSTALL/usr/config/amremote
 }
