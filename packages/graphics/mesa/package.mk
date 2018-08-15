3fcc6761c35d6462ec05f9d96941ead4058ba09c
alexelec <alexelecv5@gmail.com>
2018-08-15 09:57:49 +0300

    Merge pull request #2837 from MilhouseVH/linux41456+
    linux (RPi): update to linux-4.14.62

--- packages/graphics/mesa/package.mk
+++ packages/graphics/mesa/package.mk
@@ -1,25 +1,10 @@
-################################################################################
-#      This file is part of LibreELEC - https://libreelec.tv
-#      Copyright (C) 2018-present Team LibreELEC
-#      Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
-#
-#  LibreELEC is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 2 of the License, or
-#  (at your option) any later version.
-#
-#  LibreELEC is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
-################################################################################
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+# Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="mesa"
-PKG_VERSION="18.1.4"
-PKG_SHA256="3061488b5d85504092cf4343816cfb2d96f2ad9bc2edec31fc96933d184cf58b"
+PKG_VERSION="18.1.6"
+PKG_SHA256="bb7ce759069801804fcfb8152da3457f76cd7b4e0096e4870ff5adcb5c894289"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.mesa3d.org/"
