0cfd6e4b7a003987e6f6365d0ada04747ebe64de
alexelec <alexelecv5@gmail.com>
2018-08-17 12:10:49 +0300

    libssh: update to libssh-0.8.1
    and make vfs.sftp kodi binary addon working

--- packages/mediacenter/kodi-binary-addons/vfs.sftp/package.mk
+++ packages/mediacenter/kodi-binary-addons/vfs.sftp/package.mk
@@ -1,15 +1,16 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
+# Copyright (C) 2018-present CoreELEC (https://coreelec.org)
 
 PKG_NAME="vfs.sftp"
 PKG_VERSION="ac78185"
 PKG_SHA256="ccaacba75785667d594c2fa9b29229a24cffbc3f0400d01bf073e8d59f4096c8"
-PKG_REV="1"
+PKG_REV="2"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://kodi.tv"
 PKG_URL="https://github.com/notspiff/vfs.sftp/archive/$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain kodi-platform"
+PKG_DEPENDS_TARGET="toolchain kodi-platform libssh"
 PKG_SECTION=""
 PKG_SHORTDESC="vfs.sftp"
 PKG_LONGDESC="vfs.sftp"
