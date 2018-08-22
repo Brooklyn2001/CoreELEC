146ed453a2dc1d80669127ae814a43dc408d5ada
alexelec <alexelecv5@gmail.com>
2018-08-22 12:12:41 +0300

    brcmfmac_sdio-firmware-rpi: update to brcmfmac_sdio-firmware-rpi-8c1e2bf

--- packages/linux-firmware/brcmfmac_sdio-firmware-rpi/package.mk
+++ packages/linux-firmware/brcmfmac_sdio-firmware-rpi/package.mk
@@ -1,25 +1,10 @@
-################################################################################
-#      This file is part of LibreELEC - https://libreelec.tv
-#      Copyright (C) 2016-present Team LibreELEC
-#      Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
-#
-#  LibreELEC is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 2 of the License, or
-#  (at your option) any later version.
-#
-#  LibreELEC is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
-################################################################################
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+# Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="brcmfmac_sdio-firmware-rpi"
-PKG_VERSION="dd61ee1"
-PKG_SHA256="e8d083b04e405aef5601b485a927d06398642e50c2492a98d10e1f6bcf20d75f"
+PKG_VERSION="8c1e2bff1da9850f68efcfff3da5d939ec27a2ee"
+PKG_SHA256="155ebd5f08b819e0ce4e1950fcc972b2086cee3c16d36aba348beba1910c1fd2"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/LibreELEC/LibreELEC.tv"
