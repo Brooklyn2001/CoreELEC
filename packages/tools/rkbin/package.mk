172bffb677ca67973cd47e8965b2800f84de7933
alexelec <alexelecv5@gmail.com>
2018-08-22 13:01:25 +0300

    rkbin: update to b3a2661

--- packages/tools/rkbin/package.mk
+++ packages/tools/rkbin/package.mk
@@ -1,24 +1,10 @@
-################################################################################
-#      This file is part of LibreELEC - https://libreelec.tv
-#      Copyright (C) 2017-present Team LibreELEC
-#
-#  LibreELEC is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 2 of the License, or
-#  (at your option) any later version.
-#
-#  LibreELEC is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
-################################################################################
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="rkbin"
-PKG_VERSION="f64ded6"
-PKG_SHA256="7b858ac964058da83cd96314184d2c5f834a9b2cc6b805be424a661fd9836b54"
+# Version is: Kwiboo/tag:libreelec-b3a2661
+PKG_VERSION="b3a2661830dd7e1800b755373b02ac892863ef9b"
+PKG_SHA256="e69637c354afb008373eea5d4fe58dc17161e3a6091cefc0a76be247a595ce8b"
 PKG_ARCH="arm aarch64"
 PKG_LICENSE="nonfree"
 PKG_SITE="https://github.com/rockchip-linux/rkbin"
