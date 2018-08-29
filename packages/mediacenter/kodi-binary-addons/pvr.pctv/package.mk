46731c6816681cd6f6efe1a82b9a2a24d1fb035b
alexelec <alexelecv5@gmail.com>
2018-08-29 12:26:48 +0300

    kodi-binary-addons: update to latest versions

--- packages/mediacenter/kodi-binary-addons/pvr.pctv/package.mk
+++ packages/mediacenter/kodi-binary-addons/pvr.pctv/package.mk
@@ -1,25 +1,10 @@
-################################################################################
-#      This file is part of LibreELEC - https://libreelec.tv
-#      Copyright (C) 2018-present Team LibreELEC
-#      Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
-#
-#  LibreELEC is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 2 of the License, or
-#  (at your option) any later version.
-#
-#  LibreELEC is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
-################################################################################
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+# Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="pvr.pctv"
-PKG_VERSION="32d4b66"
-PKG_SHA256="509d9fe09208d60f80e064aa5a13cfcf0c1be435e49f95628f159d6647f26a49"
+PKG_VERSION="9d95bab1d49b80bb11b86373ca7a44838d12adb5"
+PKG_SHA256="ebbf7df31a8a106d87edd4fe5e07b8c0f77c0f36091af0830b0a146fd50f3bdc"
 PKG_REV="2"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
--- packages/mediacenter/kodi-binary-addons/pvr.vuplus/package.mk
+++ packages/mediacenter/kodi-binary-addons/pvr.vuplus/package.mk
@@ -1,25 +1,10 @@
-################################################################################
-#      This file is part of LibreELEC - https://libreelec.tv
-#      Copyright (C) 2018-present Team LibreELEC
-#      Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
-#
-#  LibreELEC is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 2 of the License, or
-#  (at your option) any later version.
-#
-#  LibreELEC is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
-################################################################################
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+# Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="pvr.vuplus"
-PKG_VERSION="75179a3"
-PKG_SHA256="4aa46b8de08e8d1c14400cbb5e9ca7c91b04498fcf60d9ac1b09af2aee6e9a91"
+PKG_VERSION="016a4ff6d8c607a0119e825605f8f83a073fc662"
+PKG_SHA256="c9a42c2f208cb4c0b833c93397e47463b1314099203dce4e64f3967ff79907dd"
 PKG_REV="2"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
