449ae24e8b754c5f966afe5129b8ed68379d5483
alexelec <alexelecv5@gmail.com>
2018-11-13 12:57:39 +0200

    kodi-binary-addons: update to latest versions

--- packages/mediacenter/kodi-binary-addons/pvr.iptvsimple/package.mk
+++ packages/mediacenter/kodi-binary-addons/pvr.iptvsimple/package.mk
@@ -4,13 +4,13 @@
 # Copyright (C) 2011-present Alex@ELEC (http://alexelec.in.ua)
 
 PKG_NAME="pvr.iptvsimple"
-PKG_VERSION="8b7c701cfa685bc01445f68b63a96b5d7a6367f5"
-PKG_SHA256="01cae25bd7ba35f1982bcaea2298ec35542a67f319e692ac7abfe1e3186d7e6b"
+PKG_VERSION="90a8d6f3b0b65ed3f61dfcbfbd4d75175688bec0"
+PKG_SHA256="a6b47e0ae2b15ab8070640d1dd98a3d940a3882b6d8b6e43097086de20df5648"
 PKG_REV="3"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
-PKG_SITE="http://www.kodi.tv"
-PKG_URL="https://github.com/kodi-pvr/pvr.iptvsimple/archive/$PKG_VERSION.tar.gz"
+PKG_SITE="https://github.com/kodi-pvr/pvr.iptvsimple"
+PKG_URL="$PKG_SITE/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain kodi-platform zlib rapidxml"
 PKG_SECTION=""
 PKG_SHORTDESC="pvr.iptvsimple"
