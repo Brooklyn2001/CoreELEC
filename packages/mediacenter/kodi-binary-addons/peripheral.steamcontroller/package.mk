45b8ce223d637af1f8e9194fb291ef1164491849
alexelec <alexelecv5@gmail.com>
2018-11-28 12:07:12 +0200

    kodi-binary-addons: update to latest versions

--- packages/mediacenter/kodi-binary-addons/peripheral.steamcontroller/package.mk
+++ packages/mediacenter/kodi-binary-addons/peripheral.steamcontroller/package.mk
@@ -1,10 +1,9 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
-# Copyright (C) 2009-2016 Lukas Rusak (lrusak@libreelec.tv)
-# Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
+# Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="peripheral.steamcontroller"
-PKG_VERSION="ea34539"
-PKG_SHA256="cffac3516af2971e739974175d27cf36d1f9dd255df058a169805355b176eef7"
+PKG_VERSION="a29119e9bc232fdd4f3b6287eb699ecd43147533"
+PKG_SHA256="6bc4470c67499a51cf8e4751cf26d8d888091e2b0cb0f95c75f57f196f817cb6"
 PKG_REV="102"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
