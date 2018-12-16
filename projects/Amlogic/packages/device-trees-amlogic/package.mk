202049964d26e16e76355688ee03e541c3b174a2
alexelec <alexelecv5@gmail.com>
2018-12-16 16:24:24 +0200

    device-trees-amlogic: new device MeCool M8S Plus DVB

--- projects/Amlogic/packages/device-trees-amlogic/package.mk
+++ projects/Amlogic/packages/device-trees-amlogic/package.mk
@@ -3,12 +3,13 @@
 # Copyright (C) 2018-present CoreELEC (https://coreelec.org)
 
 PKG_NAME="device-trees-amlogic"
-PKG_VERSION="e3f2187440c1821ba81a5616b869b9301a9c40fc"
-PKG_SHA256="ca84c35200a54de942bf88902c0aa3913126113e9cdd06df4d5c7a8e8893fdf0"
+PKG_VERSION="ccfe83cf707046c62640c4a33a0c6cbeb879b91b"
+PKG_SHA256="da9a9144d0fb5096ef693ba037853bb4e928a2fcb2faea36897f08cd8f19cd78"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/CoreELEC/device-trees-amlogic"
 PKG_URL="https://github.com/CoreELEC/device-trees-amlogic/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
+PKG_LONGDESC="Device trees for Amlogic devices." 
 PKG_IS_KERNEL_PKG="yes"
 PKG_TOOLCHAIN="manual"
 
@@ -20,7 +21,7 @@ make_target() {
   EXTRA_TREES=( \
                 gxbb_p201 gxbb_p200_1G_wetek_hub gxbb_p200_2G_wetek_play_2 \
                 gxl_p212_1g gxl_p212_2g gxl_p281_1g gxl_p212_1g_lepotato gxl_p212_2g_lepotato \
-                gxm_q200_2g gxm_q201_1g gxm_q201_2g \
+                gxm_q200_2g gxm_q201_1g gxm_q201_2g gxl_p231_1g_m8s_dvb \
 	      )
 
   # Add trees to the list
@@ -58,7 +59,7 @@ make_target() {
 
   # Compile device trees
   kernel_make $DTB_LIST_FILTERED
-  mv arch/$TARGET_KERNEL_ARCH/boot/dts/amlogic/*.dtb $PKG_BUILD
+  cp arch/$TARGET_KERNEL_ARCH/boot/dts/amlogic/*.dtb $PKG_BUILD
 
   popd > /dev/null
 }
